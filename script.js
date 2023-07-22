d2208a8b8e6bbed5ef5a8f70051c5b6e3f721560
Calvin Hughes <63322016+Ragebo123@users.noreply.github.com>
2023-07-21 17:04:35 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,57 @@
+let defusing = false;
+let defuseInterval;
+let stopInterval;
+
+const startButton = document.getElementById('startButton');
+const stopButton = document.getElementById('stopButton');
+const timerDisplay = document.getElementById('timer');
+const statusDisplay = document.getElementById('status');
+
+function startDefuse() {
+  let startTime = Date.now();
+  defusing = true;
+  startButton.innerText = 'Defusing...';
+
+  defuseInterval = setInterval(() => {
+    let currentTime = Date.now();
+    let elapsedTime = Math.floor((currentTime - startTime) / 1000);
+
+    if (elapsedTime >= 20) {
+      clearInterval(defuseInterval);
+      defusing = false;
+      statusDisplay.innerText = 'Defuser has been deactivated!';
+      startButton.innerText = 'Hold to Start Defuse (5 sec)';
+    } else {
+      let remainingTime = 20 - elapsedTime;
+      let minutes = Math.floor(remainingTime / 60);
+      let seconds = remainingTime % 60;
+      timerDisplay.innerText = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    }
+  }, 1000);
+}
+
+function stopDefuse() {
+  if (defusing) {
+    clearInterval(defuseInterval);
+    defusing = false;
+    statusDisplay.innerText = 'Defuser defuse attempt stopped!';
+    timerDisplay.innerText = '00:00';
+    startButton.innerText = 'Hold to Start Defuse (5 sec)';
+  }
+}
+
+startButton.addEventListener('mousedown', () => {
+  stopInterval = setTimeout(startDefuse, 5000);
+});
+
+startButton.addEventListener('mouseup', () => {
+  clearTimeout(stopInterval);
+});
+
+stopButton.addEventListener('mousedown', () => {
+  stopDefuse();
+});
+
+stopButton.addEventListener('mouseup', () => {
+  clearTimeout(stopInterval);
+});
